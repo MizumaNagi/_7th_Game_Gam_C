bc209359f3316947554a7ea5d973f4307aaaa670
nemonoa1021 <73728751+nemonoa1021@users.noreply.github.com>
2022-12-01 15:33:40 +0900

    [マージ禁止 ！] チェリーピック: origin/tei 12/1 15:33

---
+++ _7th_Game_Gam_C/Assets/WorkFolder/tei/Body_move.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body_move : MonoBehaviour
+{
+    public static Body_move instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    private void Start()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Body_Move()
+    {
+        transform.position += new Vector3(0, 0, 1) * Time.deltaTime;//ˆÚ“®‹——£
+    }
+}
